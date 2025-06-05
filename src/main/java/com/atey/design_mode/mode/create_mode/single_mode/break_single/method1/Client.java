2aa2d8c106e9e8c6562c7b97c915be52294b10e3
zhf2680536614 <zhf19993612440@outlook.com>
2025-06-05 15:37:01 +0800

    软件设计模式-创建者模式-单例模式-破坏单例模式-序列化和反序列话破快单例模式的解决方案1

--- src/main/java/com/atey/design_mode/mode/create_mode/single_mode/break_single/method1/Client.java
+++ src/main/java/com/atey/design_mode/mode/create_mode/single_mode/break_single/method1/Client.java
@@ -5,6 +5,7 @@ import java.io.*;
 
 public class Client {
     public static void main(String[] args) throws Exception{
+        //writeObject2File();
         readObjectFromFile();
         readObjectFromFile();
     }
--- src/main/java/com/atey/design_mode/mode/create_mode/single_mode/break_single/method1/Single.java
+++ src/main/java/com/atey/design_mode/mode/create_mode/single_mode/break_single/method1/Single.java
@@ -1,5 +1,5 @@
 package com.atey.design_mode.mode.create_mode.single_mode.break_single.method1;
-
+import java.io.Serial;
 import java.io.Serializable;
 
 //静态内部类的方式，因为JVM在加载外部类时不会加载内部类，只有内部类的方法或属性被调用时才会加载
@@ -11,6 +11,12 @@ public class Single implements Serializable {
     }
 
     public static Single getInstance(){
+        return SingleHolder.INSTANCE;
+    }
+
+    //序列化和反序列化破快单例模式的解决方案 => readResolve()在反序列化时会被执行
+    @Serial
+    private Object readResolve(){
         return SingleHolder.INSTANCE;
     }
 }
