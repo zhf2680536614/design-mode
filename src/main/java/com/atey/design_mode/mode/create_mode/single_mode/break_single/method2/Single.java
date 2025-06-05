0b7beec975e5d8e00a29f861cc1d2e09e5f4441d
zhf2680536614 <zhf19993612440@outlook.com>
2025-06-05 15:45:38 +0800

    软件设计模式-创建者模式-单例模式-破坏单例模式-反射破快单例模式的解决方案

--- src/main/java/com/atey/design_mode/mode/create_mode/single_mode/break_single/method2/Single.java
+++ src/main/java/com/atey/design_mode/mode/create_mode/single_mode/break_single/method2/Single.java
@@ -4,13 +4,24 @@ import java.io.Serializable;
 
 //静态内部类的方式，因为JVM在加载外部类时不会加载内部类，只有内部类的方法或属性被调用时才会加载
 public class Single implements Serializable {
-    private Single(){}
 
-    private static class SingleHolder{
+    //用来标记该类的对象是否已经被创建
+    private static boolean flag = false;
+
+    private Single() {
+        synchronized (Single.class) {
+            if (flag) {
+                throw new RuntimeException("该类只允许创建一个对象");
+            }
+            flag = true;
+        }
+    }
+
+    private static class SingleHolder {
         private static final Single INSTANCE = new Single();
     }
 
-    public static Single getInstance(){
+    public static Single getInstance() {
         return SingleHolder.INSTANCE;
     }
 }
